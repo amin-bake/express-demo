d9945f5051142fd1242ea4a32a89ec1e8eb2a52d
AmIn <amincali20@gmail.com>
2024-03-24 14:26:18 +0300

    code-broken

--- server/server.js
+++ server/server.js
@@ -1,51 +1,57 @@
-const express = require('express');
+const express = require('express')
 const mongoose = require('mongoose')
-const bodyParser = require('body-parser');
-const path = require('path');
-
-const app = express();
-const PORT = process.env.PORT || 5000;
-const cors = require('cors');
-
-app.use(cors());
+const cors = require('cors')
+const UserModel = require('./models/User')
+ 
+const app = express()
+app.use(cors())
 app.use(express.json())
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-//connect to DB
-mongoose.connect('mongodb://127.0.0.1/nodeexpressdb',{
+ 
+ 
+mongoose.connect('mongodb://127.0.0.1/crud-app',{
     useNewUrlParser: true, 
     useUnifiedTopology: true
 })
 .then(db => console.log('DB is connected'))
 .catch(err => console.log(err));
-
-
-// API endpoints for tasks
-let tasks = [];
-
+ 
 app.get('/', (req, res) => {
-  res.json(tasks);
-});
-
-app.post('/api/tasks', (req, res) => {
-  const newTask = req.body.task;
-  tasks.push(newTask);
-  res.status(201).json({ success: true, message: 'Task added successfully' });
-});
-
-app.delete('/api/tasks/:index', (req, res) => {
-  const index = req.params.index;
-  tasks.splice(index, 1);
-  res.status(200).json({ success: true, message: 'Task deleted successfully' });
-});
-
-// Handle React routing, return all requests to React app
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, '../todo/public', 'index.html'));
-// });
-
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+    UserModel.find()
+    .then(users => res.json(users))
+    .catch(err => res.json(err))
+})
+ 
+ 
+app.get('/get/:id', (req, res) => {
+    const id = req.params.id
+    UserModel.findById({_id: id})
+    .then(post => res.json(post))
+    .catch(err => console.log(err))
+})
+ 
+app.post('/create', (req, res) => {
+    UserModel.create(req.body)
+    .then(user => res.json(user))
+    .catch(err => res.json(err))
+})
+ 
+app.put('/update/:id', (req, res) => {
+    const id = req.params.id;
+    UserModel.findByIdAndUpdate({_id: id}, {
+        name: req.body.name,
+        email: req.body.email,
+        age: req.body.age
+    }).then(user => res.json(user))
+    .catch(err => res.json(err))
+})
+ 
+app.delete('/deleteuser/:id', (req, res) => {
+    const id = req.params.id;
+    UserModel.findByIdAndDelete({_id: id})
+    .then(response => res.json(response))
+    .catch(err => res.json(err))
+})
+ 
+app.listen(3001, () => {
+    console.log("Server is Running");
+})
--- todo/src/App.js
+++ todo/src/App.js
@@ -1,122 +1,19 @@
-// import logo from './logo.svg';
-// import './App.css';
-// import React, { useState, useEffect} from 'react';
-// import axios from 'axios';
-
-// function App() {
-//   const [tasks, setTasks] = useState([]);
-//   const [newTask, setNewTask] = useState('');
-
-//   const handleAddTask = () => {
-//     if (newTask.trim() !== '') {
-//       setTasks([...tasks, newTask]);
-//       setNewTask('');
-//     }
-//   };
-
-//   const handleDeleteTask = (index) => {
-//     const updatedTasks = [...tasks];
-//     updatedTasks.splice(index, 1);
-//     setTasks(updatedTasks);
-//   };
-
-//   useEffect(() => {
-//     fetch('/api/tasks')
-//       .then(response => response.json())
-//       .then(data => setTasks(data));
-//   }, []);
-
-//   return (
-//     <div className="App">
-//       <h1>To-Do List</h1>
-//       <div className="task-form">
-//         <input
-//           type="text"
-//           value={newTask}
-//           onChange={(e) => setNewTask(e.target.value)}
-//           placeholder="Enter task"
-//         />
-//         <button onClick={handleAddTask}>Add Task</button>
-//       </div>
-//       <ul>
-//         {tasks.map((task, index) => (
-//           <li key={index}>
-//             {task}
-//             <button onClick={() => handleDeleteTask(index)}>Delete</button>
-//           </li>
-//         ))}
-//       </ul>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-//using express 
-import React, { useState, useEffect } from 'react';
-import './App.css';
-
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Users from './components/Users'
+import CreateUser from './components/CreateUser'
+import UpdateUser from './components/UpdateUser'
+ 
 function App() {
-  const [tasks, setTasks] = useState([]);
-  const [newTask, setNewTask] = useState('');
-
-  useEffect(() => {
-    fetch('/api/tasks')
-      .then(response => response.json())
-      .then(data => setTasks(data));
-  }, []);
-
-  const handleAddTask = () => {
-    if (newTask.trim() !== '') {
-      fetch('/api/tasks', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ task: newTask })
-      })
-      .then(response => response.json())
-      .then(() => {
-        setTasks([...tasks, newTask]);
-        setNewTask('');
-      });
-    }
-  };
-
-  const handleDeleteTask = (index) => {
-    fetch(`/api/tasks/${index}`, {
-      method: 'DELETE'
-    })
-    .then(() => {
-      const updatedTasks = [...tasks];
-      updatedTasks.splice(index, 1);
-      setTasks(updatedTasks);
-    });
-  };
-
+ 
   return (
-    <div className="App">
-      <h1>To-Do List</h1>
-      <div className="task-form">
-        <input
-          type="text"
-          value={newTask}
-          id="input-field"
-          onChange={(e) => setNewTask(e.target.value)}
-          placeholder="Enter task"
-        />
-        <button onClick={handleAddTask}>Add Task</button>
-      </div>
-      <ul>
-        {tasks.map((task, index) => (
-          <li key={index}>
-            {task}
-            <button onClick={() => handleDeleteTask(index)}>Delete</button>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Users />}></Route>
+        <Route path='/create' element={<CreateUser />}></Route>
+        <Route path='/edit/:id' element={<UpdateUser />}></Route>
+      </Routes>
+    </BrowserRouter>
+  )
 }
-
-export default App;
+ 
+export default App
